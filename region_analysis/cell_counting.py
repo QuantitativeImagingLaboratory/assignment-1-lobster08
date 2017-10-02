5889cd8474df4747b80c910c04e09a681c10cec1
Ty Nguyen <buuty08@gmail.com>
2017-10-02 00:24:40 -0500

    Completed blob_coloring function

--- region_analysis/cell_counting.py
+++ region_analysis/cell_counting.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class cell_counting:
 
     def blob_coloring(self, image):
@@ -7,6 +9,49 @@ class cell_counting:
         return: a list of regions"""
 
         regions = dict()
+
+        # Set all values in dictionary to 0
+        for row in range(image.shape[0]):
+            for col in range(image.shape[1]):
+                regions[row, col] = 0
+
+        # Black = 1
+        # White = 0
+        label = 1
+        for row in range(image.shape[0]):
+            for col in range(image.shape[1]):
+
+                # Get pixel value at coordinate (row,col) or (y,x)
+                pixel = np.average(image[row, col])
+
+                # Get pixel value on the left of the coordinate (row,col)
+                if (col == 0):
+                    continue
+                else:
+                    upPixel = np.average(image[row, col - 1])
+
+                # Get pixel value above of the coordinate (row,col)
+                if (row == 0):
+                    continue
+                else:
+                    leftPixel = np.average(image[row - 1, col])
+
+                # Assign label at coordinate (row,col)
+                if (pixel == 255 and leftPixel == 0 and upPixel == 0):
+                    regions[row, col] = label
+                    label += 1
+
+                if (pixel == 255 and leftPixel == 0 and upPixel == 255):
+                    regions[row, col] = regions[row - 1, col]
+
+                if (pixel == 255 and leftPixel == 255 and upPixel == 0):
+                    regions[row, col] = regions[row, col - 1]
+
+                if (pixel == 255 and leftPixel == 255 and upPixel == 255):
+                    regions[row, col] = regions[row - 1, col]
+
+                if (regions[row, col - 1] == regions[row - 1, col]):
+                    regions[row, col - 1] = regions[row - 1, col]
 
         return regions
 
